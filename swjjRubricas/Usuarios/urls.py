44f27afb6697ccdf5d0ccc74b19e52fe3622b054
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-04-23 17:15:32 -0400

    se agrega el link a cursos, en el landing page del admin

--- swjjRubricas/Usuarios/urls.py
+++ swjjRubricas/Usuarios/urls.py
@@ -5,5 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('menu/', views.menu, name='menu'),
+    path('landing_admin/', views.menu, name='landing_admin'),
+
+    path('cursos_admin/', views.cursos_admin, name='cursos_admin'),
+    path('evaluaciones_admin/', views.evaluaciones_admin, name='evaluaciones_admin'),
+    path('evaluadores_admin/', views.evaluadores_admin, name='evaluadores_admin'),
+    path('rubricas_admin/', views.rubricas_admin, name='rubricas_admin'),
 ]
--- swjjRubricas/Usuarios/views.py
+++ swjjRubricas/Usuarios/views.py
@@ -5,4 +5,13 @@ def login(request):
     return render(request, 'Usuarios/login.html')
 
 def menu(request):
-    return render(request, 'Usuarios/menu.html')
+    return render(request, 'Usuarios/Admin/landingAdmin.html')
+
+def cursos_admin(request):
+    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+def evaluaciones_admin(request):
+    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+def evaluadores_admin(request):
+    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+def rubricas_admin(request):
+    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
