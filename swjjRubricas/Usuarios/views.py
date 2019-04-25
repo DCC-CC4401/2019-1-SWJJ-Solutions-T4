6b5ebe965f01675aa00f16803a78a43137d6cf18
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-04-24 21:20:43 -0400

    ahora la pagina del admin tiene herencia (osea no se repite el codigo del menu lateral)

--- swjjRubricas/Usuarios/views.py
+++ swjjRubricas/Usuarios/views.py
@@ -5,13 +5,13 @@ def login(request):
     return render(request, 'Usuarios/login.html')
 
 def menu(request):
-    return render(request, 'Usuarios/Admin/landingAdmin.html')
+    return render(request, 'Usuarios/Admin/Landing_admin.html')
 
 def cursos_admin(request):
-    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+    return render(request, 'Usuarios/Admin/Cursos_admin.html')
 def evaluaciones_admin(request):
-    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+    return render(request, 'Usuarios/Admin/Evaluaciones_admin.html')
 def evaluadores_admin(request):
-    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+    return render(request, 'Usuarios/Admin/Evaluadores_admin.html')
 def rubricas_admin(request):
-    return render(request, 'Usuarios/Admin/Admin_interface/Cursos_admin.html')
+    return render(request, 'Usuarios/Admin/Rubricas_admin.html')
