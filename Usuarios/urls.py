38526a131f875dc5f83795d1547ae8957a0ea676
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-06-02 14:56:55 -0400

    se puede diferenciar entre admin y evaluador

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('landing_admin/<int:usuario_id>/', views.menu, name='landing_admin'),
+    path('landing_admin/<int:usuario_id>/<int:isAdmin>/', views.menu, name='landing_admin'),
 
     path('cursos_admin/<int:usuario_id>/', views.cursos_admin, name='cursos_admin'),
     path('cursos_admin_create/<int:usuario_id>/', views.cursos_admin_create, name='cursos_admin_create'),
     path('cursos_admin_delete/<int:usuario_id>/', views.cursos_admin_delete, name='cursos_admin_delete'),
-    path('evaluaciones_admin/<int:usuario_id>/', views.evaluaciones_admin, name='evaluaciones_admin'),
+    path('evaluaciones_admin/<int:usuario_id>/<int:isAdmin>/', views.evaluaciones_admin, name='evaluaciones_admin'),
     path('evaluaciones_admin_ver/<int:usuario_id>/', views.evaluaciones_admin_ver, name='evaluaciones_admin_ver'),
     path('evaluaciones_admin_create/<int:usuario_id>/', views.evaluaciones_admin_create, name='evaluaciones_admin_create'),
     path('evaluadores_admin/<int:usuario_id>/', views.evaluadores_admin, name='evaluadores_admin'),
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -28,11 +28,11 @@ def login(request):
         # Verificar si es admin
         if (Usuario_admin.objects.filter(name=username, password=password).exists()):
             user = Usuario_admin.objects.get(name=username, password=password)
-            return HttpResponseRedirect(reverse('usuarios:landing_admin', kwargs={'usuario_id': user.id}))
+            return menu(request,user.id,user.isAdmin)
 
         if (Usuario_evaluador.objects.filter(correo=username, password=password).exists()):
             user = Usuario_evaluador.objects.get(correo=username, password=password)
-            return HttpResponseRedirect(reverse('usuarios:evaluaciones_admin', kwargs={'usuario_id': user.id}))
+            return menu(request,user.id,user.isAdmin)
 
 
     return render(request, 'Usuarios/login.html')
@@ -44,9 +44,12 @@ def courses(request):
     return listaCursos
 
 
-def menu(request, usuario_id):
+def menu(request, usuario_id,isAdmin):
     # aca deberia haccerse la autenticaciondel usuario.
-    usuario = Usuario_admin.objects.get(pk=usuario_id)
+    if isAdmin==1:
+        usuario = Usuario_admin.objects.get(pk=usuario_id)
+    else:
+        usuario= Usuario_evaluador.objects.get(pk=usuario_id)
 
     return render(request, 'Usuarios/Admin/Landing_admin.html', {'usuario': usuario})
 
@@ -77,8 +80,11 @@ def cursos_admin_delete(request, usuario_id):
     return render(request, 'Usuarios/Admin/Cursos_admin_delete.html', {'usuario': usuario})
 
 
-def evaluaciones_admin(request, usuario_id):
-    usuario = Usuario_admin.objects.get(pk=usuario_id)
+def evaluaciones_admin(request, usuario_id,isAdmin):
+    if isAdmin==1:
+        usuario = Usuario_admin.objects.get(pk=usuario_id)
+    else:
+        usuario = Usuario_evaluador.objects.get(pk=usuario_id)
     return render(request, 'Usuarios/Admin/Evaluaciones_admin.html', {'usuario': usuario})
 
 
@@ -132,6 +138,7 @@ def rubricas_admin(request, usuario_id):
     if data:
         for key in data[str(usuario_id)]:
             rubricsNames.append(key)
+
     print(rubricsNames)
 
 
